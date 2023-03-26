338a24dcf41f93787e97a90cc67608dff7bb64a9
Sivtout666 <102421687+Sivtout666@users.noreply.github.com>
2023-03-26 12:30:38 +0200

    Create nuage.js
    Ajout des nuages

---
+++ nuage.js
@@ -0,0 +1,32 @@
+import { noise } from "./perlin.js"
+
+function generer_nuage() {
+    let rayon_nuage = 250;
+    let canvas = document.getElementById("nuage");
+    let ctx = canvas.getContext("2d");
+    if(ctx) { 
+        for(let x = 0; x < 500; x++) {
+            for(let y = 0; y < 500; y++) {
+                let n = noise(x * 0.09, y * 0.09);
+
+                if(n > 0.85) { 
+                    ctx.fillStyle = "rgba( 240, 240, 240, 1)";
+                    ctx.fillRect(x, y, 1 / 1.25, 1 / 1.25);
+                }
+                else if(n > 0.8){
+                    ctx.fillStyle = "rgba( 230, 230, 230, 1)";
+                    ctx.fillRect(x, y, 1 / 1.5, 1 / 1.5);
+                }
+                else if(n > 0.7){
+                    ctx.fillStyle = "rgba( 220, 220, 220, 1)";
+                    ctx.fillRect(x, y, 1 / 2, 1 / 2);
+                }
+
+            }
+        }
+    }
+}
+
+window.addEventListener("load", generer_nuage);
+
+export { generer_nuage }
