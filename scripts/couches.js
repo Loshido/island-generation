d6bf1b244a4151c4c10e481327819c11bf1b240b
Loshido <loshido@pm.me>
2023-05-19 13:56:50 +0200

    Instruction

--- scripts/couches.js
+++ scripts/couches.js
@@ -61,15 +61,36 @@ function paramsNode(inputs) {
     colorInput.value = rgbToHex(...color)
     rangeInput.value = range
 
+    // Il faudrait que lorsque les valeurs de minInput, colorInput, rangeInput changent, 
+    // les valeurs de la variable "couches" changent aussi.
+    // On peut utiliser l'événement "input"
+
+    // De plus attention j'ai mis x100 à l'indice pour que ça soit plus lisible. 
+    // Ducoup faut bien diviser par 100 lorsqu'on met dans "couches"
+
+    // ensuite il faut faire en sorte que lorsque que l'on change la valeur d'un indice, l'indice au dessus sois inférieur 
+    // donc par exemple if(indice > indice_dessus) indice = indice - 5
+
+    // Enfin faudra changer la hauteur de l'élément en fonction de ses indices 
+    // avec node.style.height = `${130 + (inputs.max - inputs.min) * 200}px`
+    // tel que node est obtenable par document.getElementById("nom_de_la_couche")
+    // et que inputs.max et inputs.min sont les indices de la couche
+    // 130 c'est la valeur minimum et 200 c'est pour multiplier par 100 et 2 pour avoir une hauteur correcte
+
+    // Et faudra également changer le dégradé de couleur de la couche à chaque fois qu'on change la couleur ou le "range" (juste à coté de la couleur)
+    // avec const { bg, grad } = gradient(inputs.color, inputs.range)
+    // et ensuite rect.style.background = bg et rect.style.backgroundImage = grad
+    // tel que rect est obtenable par document.querySelector("#nom_de_la_couche > .rect")
+
     return params
 }
 
+// Fonction qui créer un dégradé de couleur de la couleur de la couche de base
+// et de la couleur de la couche lorsqu'elle est à son indice minimum
 function gradient(color, range) {
     const rgb = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
     const bg = `background: ${rgb};`
-    const grad = `linear-gradient(0, ${rgb}, rgb(${
-        color[0] - range }, ${ color[1] - range
-    }, ${ color[2] - range }))`
+    const grad = `linear-gradient(0, ${rgb}, rgb(${color[0] - range }, ${ color[1] - range}, ${ color[2] - range }))`
 
     return { bg, grad }
 }
@@ -86,6 +107,9 @@ function createCouche(name, inputs) {
     const { bg, grad } = gradient(inputs.color, inputs.range)
     rect.style.background = bg
     rect.style.backgroundImage = grad
+
+    // On change la hauteur d'élément en fonction de ses indices
+    // ça permet de pouvoir visualiser la taille des couches
     node.style.height = `${130 + (inputs.max - inputs.min) * 200}px`
 
     const data = document.createElement("div")
